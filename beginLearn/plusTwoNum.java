66f67206093a32c0337e4ea72aac25d2db6777fd
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-10-21 23:10:38 +0300

    Added one simple program

---
+++ beginLearn/plusTwoNum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class plusTwoNum {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("result " + a + " + " + b + "  = " + (a+b));
+    }
+}
