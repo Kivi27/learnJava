ef92b2941b185b9e00f4c5c6a822574a908caadb
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-11-12 22:28:39 +0200

    Learn simple constructor

--- beginLearn/PatientDemo.java
+++ beginLearn/PatientDemo.java
@@ -2,22 +2,20 @@ class Patient {
     String name;
     int age;
     float height;
+
+    public Patient(String name, int age, float height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
 }
 
 public class PatientDemo {
     public static void main(String[] args) {
-        Patient john = new Patient();
-        john.name = "John";
-        john.age = 30;
-        john.height = 180;
-
+        Patient john = new Patient("Heinrich",40,182.02f);
         System.out.println(john.name + " " + john.age + " " + john.height);
 
-        Patient alice = new Patient();
-        alice.name = "Alice";
-        alice.age = 22;
-        alice.height = 165;
-
+        Patient alice = new Patient("Mary",33,171.5f);
         System.out.println(alice.name + " " + alice.age + " " + alice.height);
     }
 }
