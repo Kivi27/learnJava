770e4b64f526c413d2dd449f529ea87d205e8439
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-11-15 19:51:44 +0200

    improve solved

--- solvedTask/Matrix.java
+++ solvedTask/Matrix.java
@@ -10,26 +10,19 @@ import java.util.Scanner;
 
 class Matrix {
     public static void main(String[] args) {
-        // put your code here
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        scanner.close();
-        int[][] array = new int[size][size];        
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-               if (i == j) {
-                   array[i][j] = 0;
-               } else if (j > i) {
-                   array[i][j] = j - i;
-               } else {
-                   array[i][j] = i - j;
-               }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] nums = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                nums[i][j] = Math.abs(i - j);
             }
         }
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%1d ", nums[i][j]);
             }
             System.out.println();
         }
