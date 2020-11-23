16c041ff8ff9cff2885f53a92113fbd9f4d26ab2
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-11-23 21:39:59 +0200

    Added new program for test

---
+++ beginLearn/TestPrintf.java
@@ -0,0 +1,8 @@
+class TestPrintf {
+    public static void main(String[] args) {
+        System.out.printf("Display a Integer %d%n",15000);
+        System.out.printf("Display a Character %c%n",'c');
+        System.out.printf("Display a Floating-point Number %f%n",123.45);
+        System.out.printf("Display a String %s%n","String");
+    }
+}
