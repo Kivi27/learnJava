6fa8e8928acdcca150b88fd4f50a5ce7811865d9
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-12-01 21:47:49 +0200

    Learned new method System it's nanoTime()

---
+++ differentExample/beginLearn/TestSystemNanoTime.java
@@ -0,0 +1,6 @@
+class TestSystemNanoTime {
+    public static void main(String[] args) {
+        long pseudoRandomNumber = System.nanoTime();
+        System.out.println(pseudoRandomNumber);
+    }
+}
